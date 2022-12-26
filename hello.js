eb4ff19df08b877327aee08a1b6d863c4ae18a5e
namraaslam <namra.aslam@ucalgary.ca>
2022-12-26 02:26:09 -0700

    return and console.log test

--- hello.js
+++ hello.js
@@ -1,6 +1,18 @@
-const sayHello  = function (name) {
+/* const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 
 sayHello();
-sayHello("namra");
+sayHello("namra"); */
+
+/* const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('Namra'); */
+
+/* nothing gets printed to console
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('Namra'); */
