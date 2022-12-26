c663f97fd6220b58e2b380e033cbf1c2ecf350b6
namraaslam <namra.aslam@ucalgary.ca>
2022-12-26 02:38:33 -0700

    isOdd exercise

---
+++ odd.js
@@ -0,0 +1,6 @@
+const isOdd = function (num) {
+  return num % 4 === 0 ? false : true
+}
+// code given my lighthouse to check if fucntion works correctly
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
