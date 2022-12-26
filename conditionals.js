2fdadf7195fdf9504d5de35c47a2d034aaf68a43
namraaslam <namra.aslam@ucalgary.ca>
2022-12-26 00:57:23 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,35 @@
-const temperature = -16;
+/* const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+} */
+
+/* const temperature = -16;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
-} else if (temperature < 13) {
+} else if (temperature < 11) {
   console.log("Short sleeves won't cut it!");
 } else {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!"); */
+
+const raining = true;
+const cold = false;
+
+/* if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!"); */
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
